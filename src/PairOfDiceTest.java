7de4f742560e7e76c157bbba2a9c156c586e4cd4
m4anuel <manuel.flueckiger.mf@gmail.com>
2022-10-28 14:40:41 +0200

    TheorieAufgaben gelösst

--- src/PairOfDiceTest.java
+++ src/PairOfDiceTest.java
@@ -2,5 +2,7 @@ public class PairOfDiceTest {
     public static void main(String[] args) {
         PairOfDice pair = new PairOfDice();
         System.out.println(pair.getPoints());
+        pair.throwDice();
+        System.out.println(pair.getPoints());
     }
 }
--- src/TheorieAufgaben.java
+++ src/TheorieAufgaben.java
@@ -2,16 +2,30 @@ public class TheorieAufgaben {
     public static void main(String[] args) {
 
         //alle zum durchprobieren...
+        System.out.println("Aufgabe 1");
         System.out.print("a)");
         theorie1(5, 4);
         System.out.print("\nb)");
-        theorie1(8,4);
+        theorie1(8, 4);
         System.out.print("\nc)");
-        theorie1(10,11);
+        theorie1(10, 11);
         System.out.print("\nd)");
-        theorie1(10,12);
-        System.out.print("\ne)");
-        theorie1(10,13);
+        theorie1(10, 12);
+        System.out.println("\ne)");
+        theorie1(10, 13);
+        System.out.println("Aufgabe 2");
+        System.out.println(gregorianYear(400));
+        System.out.println("\nAufgabe 3");
+        System.out.println(isIsosceles(3, 3, 2));
+        System.out.println("Aufgabe 4 advanced");
+        System.out.println(count("Manuel Flückiger", 'e'));
+        System.out.println("Aufgabe 5");
+        theorie5();
+        System.out.println("Theorie 6");
+        theorie6();
+        System.out.println("Theorie 7");
+        theorie7();
+
     }
     public static void theorie1(int num1, int num2){
         if (num1 >= num2) {
@@ -25,5 +39,39 @@ public class TheorieAufgaben {
             System.out.print("5 ");
             System.out.print("6 ");
         }
+        else {
+            System.out.print("7 ");
+            System.out.print("8 ");
+        }
+    }
+    public static boolean isIsosceles(int x, int y, int z){
+        return (!(x == y && y == z ) && (x == y || y == z || z == x ));
+    }
+    public static boolean gregorianYear(int year){
+        return (year%4==0&&(year%100!=0||year%400==0));
+    }
+    public static int count(String name, char c){
+        return name.length()==0 ? 0 : (name.charAt(0)==c ? 1 : 0) + count(name.substring(1),c);
+    }
+    public static void theorie5(){
+        int low = 0, high = 5;do {
+            System.out.println(low);
+            low++;
+        } while (low < high);
+    }
+    public static void theorie6(){
+        for (int i = 1; i <= 3; i++) {
+            for (int j = i; j > 0; j--)
+                System.out.println(i + " " + j);
+        }
+    }
+    public static void theorie7(){
+        int value = 0;
+        int num = 10;
+        while(num<=40){
+            value+=num;
+            num+=10;
+        }
+        System.out.println(value);
     }
 }
