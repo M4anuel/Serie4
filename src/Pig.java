817b94c106b41834b74598fba14af0bea7154734
m4anuel <manuel.flueckiger.mf@gmail.com>
2022-10-31 11:31:15 +0100

    Pig Spiel geht nicht Player ist Hilfsklasse für dieses Spiel

--- src/Pig.java
+++ src/Pig.java
@@ -10,71 +10,53 @@ public class Pig {
                 " indem er y oder n in die Konsole eingibt.\n Dabei werden gewürfelte Punkte akkumuliert." +
                 " Sollte eine 1 gewürfelt werden, sind " +
                 "alle, in dem Zug gewonnenen Punkte, verloren.");
-        int a, b;
-        a = b = 0;
-        int temp = 0;
-        boolean player = false;
-        String answer;
-        PairOfDice dices = new PairOfDice();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Will Spieler 1 würfeln? (y/n)");
 
-        while(a<20 || b<20){
-            if (!player){
-                dices.throwDice();
-                answer = scanner.next();
-                if (answer.equals("y")){
-                    dices.throwDice();
-                    System.out.println("Spieler 1 hat "+ dices.getPoints()+" gewürfelt");
-                    if (isOne(dices)){
-                        System.out.println("aber dabei eine Eins gewürfelt");
-                        System.out.println("Spieler 2 zum Würfeln y eingeben");
-                        temp = 0;
-                        player = true;
-                    }
-                    else{
-                        temp+=dices.getPoints();
-                        System.out.println("Spieler 1 hat jetzt "+a+temp+" Punkte.");
-                        System.out.println("nochmals würfeln (y/n)?");
-                    }
-                }
-                else if (answer.equals("n")){
-                    a+=temp;
-                    temp=0;
-                    System.out.println("Spieler 1 hat jetzt "+a+" Punkte.");
-                    System.out.println("Spieler 2, willst du würfeln? (y/n)");
-                    player = true;
-                }
+        PairOfDice dices = new PairOfDice();
+        Player player1 = new Player("Manuel Player 1", 0);
+        Player player2 = new Player("Flückiger Pla 2",0);
+        int temp;
+        while(player1.getPoints()<=20 && player2.getPoints()<=20){
+            temp = 0;
+            player1.addPoints(piggyBack(player1,dices,temp));
+            temp = 0;
+            player2.addPoints(piggyBack(player2,dices,temp));
+        }
+
+    }
+    public static int piggyBack(Player p, PairOfDice dices, int temp){
+        System.out.println(p.getName()+" ist dran. Möchtest du würfeln?");
+        Scanner scanner = new Scanner(System.in);
+        String answer = scanner.next();
+        if (answer.equals("n")){ //if a player is satisfied with the gained points he can choose no to save his points
+            System.out.println(p.getName()+" hat jetzt "+p.getPoints()+" Punkte.");
+            sleeP();
+            return temp;
+        }
+        else if(answer.equals("y")){//if the answer is y, dices are thrown
+            dices.throwDice();
+            temp += dices.getPoints();
+            System.out.println(p.getName()+" hat "+ dices.getPoints() +" gewürfelt");
+            if(isOne(dices)){ //should there be a one now, the player doesn't get any points, and it's the other player's turn
+                System.out.println("aber dabei eine Eins gewürfelt");
+                System.out.println("anderer Spieler würfelt:");
+                sleeP();
+                return 0;
             }
-            if (player){
-                dices.throwDice();
-                answer = scanner.next();
-                if (answer.equals("y")){
-                    dices.throwDice();
-                    System.out.println("Spieler 2 hat "+ dices.getPoints()+" gewürfelt");
-                    if (isOne(dices)){
-                        System.out.println("aber dabei eine Eins gewürfelt");
-                        System.out.println("Spieler 1 zum würfeln y eingeben");
-                        temp = 0;
-                        player = false;
-                    }
-                    else{
-                        temp+=dices.getPoints();
-                        System.out.println("Spieler 2 hat jetzt "+b+temp+" Punkte.");
-                        System.out.println("nochmals würfeln (y/n)?");
-                    }
-                }
-                else if (answer.equals("n")){
-                    a+=temp;
-                    temp=0;
-                    System.out.println("Spieler 2 hat jetzt "+b+" Punkte.");
-                    System.out.println("Spieler 1, willst du würfeln? (y/n)");
-                    player = false;
-                }
+            else{//if the next throw isn't 1, this rounds addition goes up by the the amount of points on the dices
+                System.out.println(p.getName()+" hat jetzt "+(p.getPoints()+temp)+" Punkte.");
+                return piggyBack(p, dices, temp);
             }
         }
+        return 0;
     }
     public static boolean isOne(PairOfDice p){
         return p.getDice1().getPoints() == 1 || p.getDice2().getPoints() == 1;
+    }
+    public static void sleeP(){
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ src/Player.java
@@ -0,0 +1,28 @@
+public class Player {
+    private String name;
+    private int points;
+
+    public Player(String name, int points){
+        this.name = name;
+        this.points = points;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+    public void addPoints(int i){
+        this.points+= i;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+}
